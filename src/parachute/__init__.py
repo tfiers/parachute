4068d5077c4884dfb7e181add1905a2d0ddda623
Tomas Fiers <tomas.fiers@gmail.com>
2018-09-11 03:46:54 +0200

    Clean namespace

--- src/parachute/__init__.py
+++ src/parachute/__init__.py
@@ -1,3 +1,3 @@
-from .argcheck import *
-from .types import *
-from .util import *
+from .argcheck import input_validation, ArgumentError, check_arg, validate
+from .types import Either
+from .util import make_docstring
