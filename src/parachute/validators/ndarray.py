66d2c05a04ca62a8f8c03bfabea8693bed50cc8b
Tomas Fiers <tomas.fiers@gmail.com>
2018-09-12 15:29:35 +0200

    Tensor subclasses np.ndarray for IDE code completion

--- src/parachute/validators/ndarray.py
+++ src/parachute/validators/ndarray.py
@@ -56,13 +56,26 @@ TensorType = Union[np.ndarray, PythonScalar, PythonVector, PythonMatrix]
 DTypeSpec = Union[bool, int, float, complex]
 
 
-class Tensor(Validator):
+# Syntax to correctly subclass np.ndarray thanks to
+# https://sourceforge.net/p/numpy/mailman/message/12594316/
+class Tensor(Validator, np.ndarray):
+    """
+    Checks whether the argument is a scalar, a numeric vector, a numeric matrix,
+    or in general, a numeric tensor, of the right shape and data type.
+
+    Subclasses np.ndarray to enable code completion in IDE's.
+    """
+
+    def __new__(cls, *args, **kwargs) -> np.ndarray:
+        obj = np.ndarray.__new__(cls, shape=None)
+        return obj
+
     def __init__(
-        self, dtype: DTypeSpec = float, shape_spec: ShapeSpec = Arbitrary
+        self, dtype_spec: DTypeSpec = float, shape_spec: ShapeSpec = Arbitrary
     ):
         # Only for IDE's (because `is_of_valid_type` is overridden):
         self._type = TensorType
-        self.dtype = dtype
+        self.dtype_spec = dtype_spec
         self.shape_spec = shape_spec
 
     def is_of_valid_type(self, value):
@@ -73,7 +86,7 @@ class Tensor(Validator):
         try:
             if not isinstance(value, np.ndarray):
                 value = np.array(value)
-            value.astype(self.dtype, casting="safe")
+            value.astype(self.dtype_spec, casting="safe")
             return True
         except (TypeError, ValueError):
             return False
@@ -83,7 +96,12 @@ class Tensor(Validator):
 
 
 class Vector(Tensor):
+    """
+    Checks whether the argument is a numeric vector of the right data type and
+    length.
+    """
+
     def __init__(
-        self, dtype: DTypeSpec = float, length: DimSizeSpec = Arbitrary
+        self, dtype_spec: DTypeSpec = float, length: DimSizeSpec = Arbitrary
     ):
-        super().__init__(dtype, (length,))
+        super().__init__(dtype_spec, (length,))
